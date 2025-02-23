2bf54aa382eab153e755a3968d2e77255386b8a7
semsag <132539205+semsag@users.noreply.github.com>
2025-02-23 17:10:38 -0500

    cambio

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Get the client
+const mysql = require('mysql2/promise');
+const cors = require('cors')
+const session = require('express-session')
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+app.use(session({
+  secret: "12345asdfghjklñ",
+ }))
+
+// Create the connection to database
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'login',
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login',async (req, res) => { //req = request, que es una petición; res = response, respuesta
+  const datos = req.query;
+
+  // A simple SELECT query
+  try {
+    const [results, fields] = await connection.query(
+      "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
+      [datos.usuario, datos.clave]
+    );
+    if (results.length > 0) {
+      req.session.usuario = datos.usuario
+      res.status(200).send('inicio de sesion correcto')
+    }else {
+      res.status(401).send('inicio de sesion incorrecto')
+
+    }
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+app.get('/validar', (req, res) => {
+  if (req.session.usuario) {
+    res.status(200).send('sesion valida')
+  } else {
+    res.status(401).send('No autorizado')
+  }
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
